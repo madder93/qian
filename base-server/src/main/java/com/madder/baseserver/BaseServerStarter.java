048b112c14f74a1253afd2e5fb6b207f87e9f779
wangqian <514701159@qq.com>
2018-11-21 16:57:03 +0800

    新增项目

---
+++ base-server/src/main/java/com/madder/baseserver/BaseServerStarter.java
@@ -0,0 +1,12 @@
+package com.madder.baseserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseServerStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseServerStarter.class, args);
+    }
+}
