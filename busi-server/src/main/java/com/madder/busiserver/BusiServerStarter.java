ddb5dcd7d5ad728cf209e259a46d7ad9a54e3ba0
wangqian <514701159@qq.com>
2018-11-21 17:07:58 +0800

    新增项目

---
+++ busi-server/src/main/java/com/madder/busiserver/BusiServerStarter.java
@@ -0,0 +1,12 @@
+package com.madder.busiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusiServerStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusiServerStarter.class, args);
+    }
+}
